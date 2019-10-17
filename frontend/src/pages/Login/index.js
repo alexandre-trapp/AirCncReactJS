8b5b21fa60e4bbe4857a6226f888f8817a5d6302
Alexandre.Trapp <alexandre.Trapp@benner.com.br>
2019-10-16 22:22:50 -0300

    page created, obtained localStorage user, queried API spots

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 import api from '../../services/api';
 
-export default function Login() {
+export default function Login({ history }) {
 
     const [email, setEmail] = useState('');
 
     async function handleSubmit(event) {
+        event.preventDefault();
 
-    event.preventDefault();
+        const response = await api.post('/sessions', { email });
 
-    const response = await api.post('/sessions', { email });
+        const { _id } = response.data;
 
-    const { _id } = response.data;
+        localStorage.setItem('user', _id);
 
-    localStorage.setItem('user', _id);
+        history.push('/dashboard');
     }
 
     return (
